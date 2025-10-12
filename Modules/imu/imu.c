f81326e1c2773b2885054ca8e1b22a19d00b645e
Bao HQ <baofair001@gmail.com>
2025-10-12 23:06:50 +0800

    Add imu module

--- Modules/imu/imu.c
+++ Modules/imu/imu.c
@@ -1,9 +1,13 @@
 #include "imu.h"
 #include <pubsub.h>
 #include <platform.h>
+#include <stdlib.h>
 #include "icm42688p.h"
 
-#define CALIBRATION_FREQ 1000 // SCHEDULER_4KHZ
+#define IMU_FREQ 4000
+#define CALIBRATION_FREQ (IMU_FREQ * 2) // 2 seconds
+#define IMU_MOTION 100
+#define SSF_GYRO (16.4)
 
 typedef enum {
 	init = 0,
@@ -11,158 +15,104 @@ typedef enum {
 	ready,
 } imu_mode_t;
 
-static uint8_t g_i2c_buffer[32];
-static int16_t g_imu_gyro_accel[6];
-static int64_t g_imu_calibration[3];
-static float g_imu_gyro_offset[3];
-static int g_calibration_counter = 0;
-static imu_mode_t g_imu_mode = init;
+typedef icm42688p_t imu_sensor_t;
 
-static void _i2c_read(uint8_t address, uint8_t *input, uint16_t input_size,
-		uint8_t* output, uint16_t output_size) {
-	platform_i2c_write_read_dma(I2C_PORT1, address, input, input_size, output, output_size);
+typedef struct {
+	imu_sensor_t imu_sensor;
+	float gyro_accel[6];
+	float gyro_offset[6];
+	int64_t gyro_calibration[3];
+	int16_t gyro_calibration_check[3];
+	int16_t gyro_calibration_counter;
+	imu_mode_t mode;
+	topic_t topic_gyro_calibration_update;
+	topic_t topic_gyro_update;
+	topic_t topic_accel_update;
+} imu_t;
+
+static imu_t g_imu1 = {
+		{{0}, I2C_PORT1},
+		{0, 0, 0, 0, 0, 0},
+		{0},
+		{0},
+		{0},
+		0,
+		init,
+		SENSOR_IMU1_GYRO_CALIBRATION_UPDATE,
+		SENSOR_IMU1_GYRO_UPDATE,
+		SENSOR_IMU1_ACCEL_UPDATE
+};
+
+static void publish_data(imu_t *imu) {
+	imu->gyro_accel[3] = imu->gyro_accel[3] / SSF_GYRO;
+	imu->gyro_accel[4] = imu->gyro_accel[4] / SSF_GYRO;
+	imu->gyro_accel[5] = imu->gyro_accel[5] / SSF_GYRO;
+	publish(imu->topic_gyro_update, (uint8_t*)&imu->gyro_accel[3], 12);
+	publish(imu->topic_accel_update, (uint8_t*)imu->gyro_accel, 12);
 }
 
-static void _i2c_write(uint8_t address, uint8_t *data, uint16_t size) {
-	platform_i2c_write(I2C_PORT1, address, data, size);
-}
+static void calibrate(imu_t *imu) {
+	imu->gyro_calibration[0] += imu->gyro_accel[3];
+	imu->gyro_calibration[1] += imu->gyro_accel[4];
+	imu->gyro_calibration[2] += imu->gyro_accel[5];
+	imu->gyro_calibration_counter++;
 
-static void _icm42688p_init(uint8_t Ascale, uint8_t Gscale, uint8_t AODR, uint8_t GODR,
-		uint8_t aMode, uint8_t gMode, bool CLKIN) {
-	g_i2c_buffer[0] = ICM42688_INT_CONFIG1;
-	g_i2c_buffer[1] = 0x00;
-	_i2c_write(ICM42688_ADDRESS, g_i2c_buffer, 2); // select register bank 0
-
-	g_i2c_buffer[0] = ICM42688_PWR_MGMT0;
-	g_i2c_buffer[1] = gMode << 2 | aMode;
-	_i2c_write(ICM42688_ADDRESS, g_i2c_buffer, 2); // set accel and gyro modes
-	get_platform()->delay(1);
-
-	g_i2c_buffer[0] = ICM42688_ACCEL_CONFIG0;
-	g_i2c_buffer[1] = Ascale << 5 | AODR;
-	_i2c_write(ICM42688_ADDRESS, g_i2c_buffer, 2); // set accel ODR and FS
-
-	g_i2c_buffer[0] = ICM42688_GYRO_CONFIG0;
-	g_i2c_buffer[1] = Gscale << 5 | GODR;
-	_i2c_write(ICM42688_ADDRESS, g_i2c_buffer, 2); // set gyro ODR and FS
-
-	g_i2c_buffer[0] = ICM42688_GYRO_ACCEL_CONFIG0;
-	g_i2c_buffer[1] = 0x44;
-	_i2c_write(ICM42688_ADDRESS, g_i2c_buffer, 2); // set gyro and accel bandwidth to ODR/10
-
-	// interrupt handling
-	g_i2c_buffer[0] = ICM42688_INT_CONFIG;
-	g_i2c_buffer[1] = 0x18 | 0x03;
-	_i2c_write(ICM42688_ADDRESS, g_i2c_buffer, 2); // push-pull, pulsed, active HIGH interrupts
-
-	g_i2c_buffer[0] = ICM42688_INT_CONFIG1;
-	_i2c_read(ICM42688_ADDRESS, g_i2c_buffer, 1, &g_i2c_buffer[1], 1);     // clear bit 4 to allow async interrupt reset (required for proper interrupt operation)
-
-	g_i2c_buffer[0] = ICM42688_INT_CONFIG1;
-	g_i2c_buffer[1] = g_i2c_buffer[1] & ~(0x10);
-	_i2c_write(ICM42688_ADDRESS, g_i2c_buffer, 2); // clear bit 4 to allow async interrupt reset (required for proper interrupt operation)
-
-	g_i2c_buffer[0] = ICM42688_INT_SOURCE0;
-	g_i2c_buffer[1] = 0x08;
-	_i2c_write(ICM42688_ADDRESS, g_i2c_buffer, 2); // data ready interrupt routed to INT1
-
-	// Use external clock source
-	if (CLKIN) {
-		g_i2c_buffer[0] = ICM42688_REG_BANK_SEL;
-		g_i2c_buffer[1] = 0x00;
-		_i2c_write(ICM42688_ADDRESS, g_i2c_buffer, 2); // select register bank 0
-
-		g_i2c_buffer[0] = ICM42688_INTF_CONFIG1;
-		g_i2c_buffer[1] = 0x95;
-		_i2c_write(ICM42688_ADDRESS, g_i2c_buffer, 2); // enable RTC
-
-		g_i2c_buffer[0] = ICM42688_REG_BANK_SEL;
-		g_i2c_buffer[1] = 0x01;
-		_i2c_write(ICM42688_ADDRESS, g_i2c_buffer, 2); // select register bank 1
-
-		g_i2c_buffer[0] = ICM42688_INTF_CONFIG5;
-		g_i2c_buffer[1] = 0x04;
-		_i2c_write(ICM42688_ADDRESS, g_i2c_buffer, 2); // use CLKIN as clock source
+	// Check motionless
+	char failed = 0;
+	if (abs(imu->gyro_accel[3] - imu->gyro_calibration_check[0]) > IMU_MOTION) failed = 1;
+	if (abs(imu->gyro_accel[4] - imu->gyro_calibration_check[1]) > IMU_MOTION) failed = 1;
+	if (abs(imu->gyro_accel[5] - imu->gyro_calibration_check[2]) > IMU_MOTION) failed = 1;
+	imu->gyro_calibration_check[0] = imu->gyro_accel[3];
+	imu->gyro_calibration_check[1] = imu->gyro_accel[4];
+	imu->gyro_calibration_check[2] = imu->gyro_accel[5];
+	if (failed == 1) {
+		imu->mode = init;
+		uint8_t result = 0;
+		publish(imu->topic_gyro_calibration_update, (uint8_t*)&result, 1);
+		return;
 	}
 
-	g_i2c_buffer[0] = ICM42688_REG_BANK_SEL;
-	g_i2c_buffer[1] = 0x00;
-	_i2c_write(ICM42688_ADDRESS, g_i2c_buffer, 2); // select register bank 0
-
-	// For next read
-	g_i2c_buffer[0] = ICM42688_TEMP_DATA1;
-	g_i2c_buffer[1] = 0x00;
-}
-
-static void publish_data(void) {
-	publish(SENSOR_IMU_GYRO, (uint8_t*)&g_imu_gyro_accel[3], 6);
-	publish(SENSOR_IMU_ACCEL, (uint8_t*)g_imu_gyro_accel, 6);
-}
-
-static void calibrate(void) {
-	g_imu_calibration[0] += g_imu_gyro_accel[3];
-	g_imu_calibration[1] += g_imu_gyro_accel[4];
-	g_imu_calibration[2] += g_imu_gyro_accel[5];
-	g_calibration_counter++;
-
-	if (g_calibration_counter >= CALIBRATION_FREQ) {
-		g_imu_gyro_offset[0] = g_imu_calibration[0] / CALIBRATION_FREQ;
-		g_imu_gyro_offset[1] = g_imu_calibration[1] / CALIBRATION_FREQ;
-		g_imu_gyro_offset[2] = g_imu_calibration[2] / CALIBRATION_FREQ;
-		g_imu_mode = ready;
+	if (imu->gyro_calibration_counter >= CALIBRATION_FREQ) {
+		imu->gyro_offset[3] = (double)(1.0 / CALIBRATION_FREQ) * imu->gyro_calibration[0];
+		imu->gyro_offset[4] = (double)(1.0 / CALIBRATION_FREQ) * imu->gyro_calibration[1];
+		imu->gyro_offset[5] = (double)(1.0 / CALIBRATION_FREQ) * imu->gyro_calibration[2];
+		imu->mode = ready;
+		uint8_t result = 1;
+		publish(imu->topic_gyro_calibration_update, (uint8_t*)&result, 1);
 	}
 }
 
-static void imu_init(void) {
-	_icm42688p_init(AFS_2G, GFS_1000DPS, AODR_500Hz, GODR_32kHz, aMode_LN, gMode_LN, 0);
+static void imu1_calibrate(uint8_t *data, size_t size) {
+	g_imu1.gyro_calibration_counter = 0;
+	g_imu1.gyro_calibration[0] = 0;
+	g_imu1.gyro_calibration[1] = 0;
+	g_imu1.gyro_calibration[2] = 0;
+	g_imu1.gyro_calibration_check[0] = g_imu1.gyro_accel[3];
+	g_imu1.gyro_calibration_check[1] = g_imu1.gyro_accel[4];
+	g_imu1.gyro_calibration_check[2] = g_imu1.gyro_accel[5];
+	g_imu1.mode = calibrating;
 }
 
-static void imu_loop(uint8_t *data, size_t size) {
-	uint8_t *buffer = &g_i2c_buffer[2];
-	_i2c_read(ICM42688_ADDRESS, g_i2c_buffer, 1, buffer, 14);
+static void imu1_loop(uint8_t *data, size_t size) {
+	icm42688p_read(&g_imu1.imu_sensor, g_imu1.gyro_accel);
 
-	// left-, right+, back+, front-
-	int16_t ax = (int16_t) (buffer[2] << 8 | buffer[3]);
-	int16_t ay = (int16_t) (buffer[4] << 8 | buffer[5]);
-	int16_t az = (int16_t) (buffer[6] << 8 | buffer[7]);
-
-	// left-, right+, back-, front+
-	int16_t gx = (int16_t) (buffer[8] << 8 | buffer[9]);
-	int16_t gy = (int16_t) (buffer[10] << 8 | buffer[11]);
-	int16_t gz = (int16_t) (buffer[12] << 8 | buffer[13]);
-
-	g_imu_gyro_accel[0] = ax;
-	g_imu_gyro_accel[1] = ay;
-	g_imu_gyro_accel[2] = az;
-	g_imu_gyro_accel[3] = gx;
-	g_imu_gyro_accel[4] = gy;
-	g_imu_gyro_accel[5] = gz;
-
-	if (g_imu_mode == ready) {
-		g_imu_gyro_accel[3] -= g_imu_gyro_offset[0];
-		g_imu_gyro_accel[4] -= g_imu_gyro_offset[1];
-		g_imu_gyro_accel[5] -= g_imu_gyro_offset[2];
-		publish_data();
+	if (g_imu1.mode == ready) {
+		g_imu1.gyro_accel[0] -= g_imu1.gyro_offset[0];
+		g_imu1.gyro_accel[1] -= g_imu1.gyro_offset[1];
+		g_imu1.gyro_accel[2] -= g_imu1.gyro_offset[2];
+		g_imu1.gyro_accel[3] -= g_imu1.gyro_offset[3];
+		g_imu1.gyro_accel[4] -= g_imu1.gyro_offset[4];
+		g_imu1.gyro_accel[5] -= g_imu1.gyro_offset[5];
+		publish_data(&g_imu1);
 	}
-	else if (g_imu_mode == calibrating) {
-		calibrate();
+	else if (g_imu1.mode == calibrating) {
+		calibrate(&g_imu1);
 	}
-}
-
-static void imu_calibrate(uint8_t *data, size_t size) {
-	g_calibration_counter = 0;
-	g_imu_calibration[0] = 0;
-	g_imu_calibration[1] = 0;
-	g_imu_calibration[2] = 0;
-	g_imu_mode = calibrating;
 }
 
 void imu_setup(void) {
-	imu_init();
-	subscribe(SCHEDULER_1KHZ, imu_loop);
-	subscribe(COMMAND_CALIBRATE_IMU, imu_calibrate);
-
-	// Try to calibrate once first
-	get_platform()->delay(1000);
-	publish(COMMAND_CALIBRATE_IMU, NULL, 0);
+	icm42688p_init(&g_imu1.imu_sensor,
+			AFS_2G, GFS_2000DPS, AODR_500Hz, GODR_32kHz, aMode_LN, gMode_LN, 0);
+	subscribe(SCHEDULER_4KHZ, imu1_loop);
+	subscribe(SENSOR_IMU1_CALIBRATE_GYRO, imu1_calibrate);
 }
