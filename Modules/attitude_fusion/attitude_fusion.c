c0c75faa90c5e1e758dbe3d0f740991457b364cb
Bao HQ <baofair001@gmail.com>
2025-10-18 13:35:20 +0800

    Update robotkit lib

--- Modules/attitude_fusion/attitude_fusion.c
+++ Modules/attitude_fusion/attitude_fusion.c
@@ -3,7 +3,7 @@
 #include <platform.h>
 #include <string.h>
 #include <math.h>
-#include <filter1.h>
+#include <fusion1.h>
 
 #define MAX_IMU_ACCEL 16384
 #define DEG2RAD 0.01745329251
@@ -20,15 +20,15 @@ typedef struct {
 	double yaw;
 } angle3d_t;
 
-static filter1_t g_f11;
+static fusion1_t g_f11;
 static vector3d_t g_imu1_gyro = {0, 0, 0};
 static vector3d_t g_imu1_accel = {0, 0, MAX_IMU_ACCEL};
 
-static filter1_t g_f12;
+static fusion1_t g_f12;
 static vector3d_t g_imu2_gyro = {0, 0, 0};
 static vector3d_t g_imu2_accel = {0, 0, MAX_IMU_ACCEL};
 
-static filter1_t g_f13;
+static fusion1_t g_f13;
 static vector3d_t g_imu3_gyro = {0, 0, 0};
 static vector3d_t g_imu3_accel = {0, 0, MAX_IMU_ACCEL};
 
@@ -41,19 +41,19 @@ static void gyro_update(uint8_t *data, size_t size) {
 	double dt = 1.0 / IMU_FREQ;
 
 	vector3d_init(&g_imu1_gyro, gx, gy, gz);
-	filter1_predict(&g_f11,
+	fusion1_predict(&g_f11,
 			dt * g_imu1_gyro.x * DEG2RAD,
 			dt * g_imu1_gyro.y * DEG2RAD,
 			dt * g_imu1_gyro.z * DEG2RAD);
 
 	vector3d_init(&g_imu2_gyro, gx, gz, -gy);
-	filter1_predict(&g_f12,
+	fusion1_predict(&g_f12,
 			dt * g_imu2_gyro.x * DEG2RAD,
 			dt * g_imu2_gyro.y * DEG2RAD,
 			dt * g_imu2_gyro.z * DEG2RAD);
 
 	vector3d_init(&g_imu3_gyro, gz, gy, -gx);
-	filter1_predict(&g_f13,
+	fusion1_predict(&g_f13,
 			dt * g_imu3_gyro.x * DEG2RAD,
 			dt * g_imu3_gyro.y * DEG2RAD,
 			dt * g_imu3_gyro.z * DEG2RAD);
@@ -71,28 +71,28 @@ static void accel_update(uint8_t *data, size_t size) {
 	float az = (*(float*)&data[8]) - ACCEL_OFFSET_Z;
 
 	vector3d_init(&g_imu1_accel, ax, ay, az);
-	filter1_update(&g_f11, g_imu1_accel.x, g_imu1_accel.y, g_imu1_accel.z);
+	fusion1_update(&g_f11, g_imu1_accel.x, g_imu1_accel.y, g_imu1_accel.z);
 
 	vector3d_init(&g_imu2_accel, az, ay, -ax);
-	filter1_update(&g_f12, g_imu2_accel.x, g_imu2_accel.y, g_imu2_accel.z);
+	fusion1_update(&g_f12, g_imu2_accel.x, g_imu2_accel.y, g_imu2_accel.z);
 
 	vector3d_init(&g_imu3_accel, ax, az, -ay);
-	filter1_update(&g_f13, g_imu3_accel.x, g_imu3_accel.y, g_imu3_accel.z);
+	fusion1_update(&g_f13, g_imu3_accel.x, g_imu3_accel.y, g_imu3_accel.z);
 
 	publish(SENSOR_LINEAR_ACCEL, (uint8_t*)&g_f11.v_linear_acc, sizeof(vector3d_t));
 }
 
 static void init(void) {
-	filter1_init(&g_f11, 4.0, 0.5, IMU_FREQ);
+	fusion1_init(&g_f11, 4.0, 0.5, IMU_FREQ);
 	g_f11.accel_scale = MAX_IMU_ACCEL;
 	//g_f11.no_correction = 1;
 
-	filter1_init(&g_f12, 4.0, 0.5, IMU_FREQ);
-	filter1_predict(&g_f12, M_PI_2, 0, 0);
+	fusion1_init(&g_f12, 4.0, 0.5, IMU_FREQ);
+	fusion1_predict(&g_f12, M_PI_2, 0, 0);
 	//g_f12.no_correction = 1;
 
-	filter1_init(&g_f13, 4.0, 0.5, IMU_FREQ);
-	filter1_predict(&g_f13, 0, -M_PI_2, 0);
+	fusion1_init(&g_f13, 4.0, 0.5, IMU_FREQ);
+	fusion1_predict(&g_f13, 0, -M_PI_2, 0);
 	//g_f13.no_correction = 1;
 }
 
